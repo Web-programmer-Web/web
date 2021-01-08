2c11243bb6d61af34c798c8d048f77403a6d7e7e
Web-programmer-Web <76931512+Web-programmer-Web@users.noreply.github.com>
2021-01-08 20:58:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch('./main.wasm').then(response =>
+fetch('main.wasm').then(response =>
   response.arrayBuffer()
 ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
   instance = results.instance;
